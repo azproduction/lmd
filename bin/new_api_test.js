d877855d0be7d89f24a3f656e54a82189015eeac
azproduction <azazel.private@gmail.com>
2013-04-18 19:27:15 +0600

    basic logging

--- bin/new_api_test.js
+++ bin/new_api_test.js
@@ -1,14 +1,21 @@
 var path = require('path'),
     Build = require('../lib/build'),
-    BuildRender = require('../lib/buildRender');
+    BuildRender = require('../lib/buildRender'),
+    Logger = require('../lib/logger');
 
 var fileName = path.join(__dirname, '../examples/demos/getting_started/.lmd/index.lmd.json');
 
+var logger = new Logger();
+
+logger.log('Init build <id>' + 'index' + '</id> file <file>' + fileName + '</file>');
+
 var build = new Build('index', fileName);
 
-build.load().then(function (build) {
-    var code = new BuildRender(build).render();
-    console.log(code);
-}, function (error) {
-    console.log(error.stack);
-});
+build.withLogger(logger)
+    .load()
+    .then(function (build) {
+        var code = new BuildRender(build).render();
+        console.log(code);
+    }, function (error) {
+        console.log(error.stack);
+    });
