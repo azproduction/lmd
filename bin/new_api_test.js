802fe1bd911af4073640bd6028f1b869e3113d84
azproduction <azazel.private@gmail.com>
2013-11-01 18:23:28 +0600

    rm logger

--- bin/new_api_test.js
+++ bin/new_api_test.js
@@ -1,17 +1,12 @@
 var path = require('path'),
     Build = require('../lib/build'),
     BuildRender = require('../lib/buildRender'),
-    Logger = require('../lib/logger'),
     stringify = require('json-stringify-safe');
 
 var fileName = path.join(__dirname, '../examples/demos/getting_started/.lmd/index.lmd.json');
 
-var logger = new Logger(0);
-logger.log1('Init build %id file %file', 'index', fileName);
-
 var build = new Build('index', fileName);
-build.withLogger(logger)
-    .load()
+build.load()
     .then(function (build) {
         // TODO a way to render sub-bundles
         new BuildRender(build).render().then(function (code) {
